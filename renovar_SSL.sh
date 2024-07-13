7fcfe8d0172e7845c85120da3b02e8e5a79ba274
Rodrigo Oyarzún <113133630+royarzunma@users.noreply.github.com>
2024-07-13 16:12:40 -0300

    Delete renovar_SSL.sh

--- renovar_SSL.sh
+++
@@ -1,5 +0,0 @@
-#certbot certonly --standalone
-certbot renew --dry-run
-cat /etc/letsencrypt/live/mi.dominio.com/cert.pem > /usr/local/nginx/ssl/cert.pem
-cat /etc/letsencrypt/live/mi.dominio.com/privkey.pem > /usr/local/nginx/ssl/cert.key
-systemctL restart nginx.service
