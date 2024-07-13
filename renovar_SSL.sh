9b1b8a284ed2df28ee0def6ffa5627eba0656770
Rodrigo Oyarzún <113133630+royarzunma@users.noreply.github.com>
2024-07-13 16:38:58 -0300

    Update renovar_SSL.sh

--- renovar_SSL.sh
+++ renovar_SSL.sh
@@ -1,5 +1,9 @@
 #certbot certonly --standalone
+echo "Ingresa el nombre del dominio del streaming . . . "
+read dominio
 certbot renew --dry-run
-cat /etc/letsencrypt/live/mi.dominio.com/cert.pem > /usr/local/nginx/ssl/cert.pem
-cat /etc/letsencrypt/live/mi.dominio.com/privkey.pem > /usr/local/nginx/ssl/cert.key
+cat /etc/letsencrypt/live/$dominio/cert.pem > /usr/local/nginx/ssl/cert.pem
+cat /etc/letsencrypt/live/$dominio/privkey.pem > /usr/local/nginx/ssl/cert.key
 systemctL restart nginx.service
+echo "¡Certificados Actualizados!"
+echo ""
